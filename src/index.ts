caa6810af4c9a4321d7f1cef02a66b3685925f17
Likhan Siddiquee <likhan.siddiquee@gmail.com>
2022-06-15 22:16:29 +0200

    set version

--- src/index.ts
+++ src/index.ts
@@ -12,30 +12,62 @@ import { RemoteServiceExecutionState } from "./RemoteServiceExecutionState"
 import { RemoteServiceExecutionStateDetailed } from "./RemoteServiceExecutionStateDetailed"
 import { FileTokenStore } from "./FileTokenStore"
 import { ConsoleLogger } from "./ConsoleLogger"
+import { Capabilities, CapabilityInfo, ChargingProfile, ChargingSettings, ChargingState, CheckControlMessage, ClimateControl, CombinedRange, CombustionRange, Coordinates, Address, CurrentMileage, DepartureTime, Distance, Doors, DoorsAndWindow, DoorsAndWindows, DriverGuideInfo, ElectricRange, ElectricRangeAndStatus, ExecutionPopup, ExecutionStopPopup, FuelIndicator, FuelLevel, FuelPercentage, Issues, LastStateCall, Properties, ReductionOfChargeCurrent, Remote360, RemoteServiceRequestResponse, RequiredService, ServiceRequired, Status, ThemeSpecs, TimeStamp, Tires, TireStatus, TireStatusInfo, Vehicle, VehicleLocation, VehicleStatusBackgroundColor, Windows } from "./VehicleApiResponse"
 
-export { LogLevel, ILogger, LoggerBase, ITokenStore, Token, Regions, Account, ConnectedDrive, RemoteServices, DetailedServiceStatus, RemoteServiceExecutionState, RemoteServiceExecutionStateDetailed }
-
-// Setup the API client
-const api = new ConnectedDrive(
-    String(process.env.BMWUsername),
-    String(process.env.BMWPassword),
-    Regions.RestOfWorld,
-    new FileTokenStore(),
-    new ConsoleLogger());
-
-// Fetch a list of vehicles associated with the credentials
-
-// api.getVehicles().then(vehicles => {
-//     vehicles.forEach(vehicle => {
-//         console.log(vehicle.vin);
-//         console.log(vehicle.properties);
-//     })
-// }).catch(reason => {
-//     console.log(reason);
-// });
-
-api.lockDoors("WBAJA91000CD48772", true).then(r => {
-    console.log(r.eventId);
-}).catch(reason => {
-    console.log(reason);
-});
+export {
+    LogLevel,
+    ILogger,
+    LoggerBase,
+    ITokenStore,
+    Token,
+    Regions,
+    Account,
+    ConnectedDrive,
+    RemoteServices,
+    DetailedServiceStatus,
+    RemoteServiceExecutionState,
+    RemoteServiceExecutionStateDetailed,
+    Vehicle,
+    RemoteServiceRequestResponse,
+    Capabilities,
+    Properties,
+    DriverGuideInfo,
+    ThemeSpecs,
+    Status,
+    CapabilityInfo,
+    LastStateCall,
+    Remote360,
+    DoorsAndWindows,
+    Tires,
+    FuelLevel,
+    FuelPercentage,
+    ChargingState,
+    CombustionRange,
+    CombinedRange,
+    ElectricRange,
+    ElectricRangeAndStatus,
+    ServiceRequired,
+    VehicleLocation,
+    ClimateControl,
+    VehicleStatusBackgroundColor,
+    CurrentMileage,
+    Issues,
+    DoorsAndWindow,
+    CheckControlMessage,
+    RequiredService,
+    FuelIndicator,
+    ChargingProfile,
+    ExecutionPopup,
+    ExecutionStopPopup,
+    Doors,
+    Windows,
+    TireStatus,
+    Distance,
+    Coordinates,
+    Address,
+    ReductionOfChargeCurrent,
+    DepartureTime,
+    ChargingSettings,
+    TireStatusInfo,
+    TimeStamp
+}
