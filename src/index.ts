3f4abdea34a37175cdaacd60889e9bccdf56b54d
Likhan Siddiquee <likhan.siddiquee@gmail.com>
2024-01-03 22:06:59 +0100

    cleanup index

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import { RemoteServiceExecutionStateDetailed } from "./RemoteServiceExecutionSta
 import { FileTokenStore } from "./FileTokenStore"
 import { ConsoleLogger } from "./ConsoleLogger"
 import { Capabilities, ChargingProfile, ChargingSettings, CheckControlMessage, Coordinates, Address, DepartureTime, DriverGuideInfo,  ReductionOfChargeCurrent, RemoteServiceRequestResponse, RequiredService, TireStatus, TireStatusInfo, Vehicle, Attributes, SoftwareVersion, PuStep, MappingInfo, VehicleStatus, Time, ClimateControlState, ClimateTimer, CombustionFuelLevel, DoorsState, DriverPreferences, ElectricChargingState, TireStatuses, WindowsState, DigitalKey, RemoteChargingCommands, LocationInfo } from "./VehicleApiResponse"
+import { Utils } from "./Utils";
 
 export {
     LogLevel,
@@ -57,27 +58,6 @@ export {
     Capabilities,
     DigitalKey,
     RemoteChargingCommands,
-    RemoteServiceRequestResponse
+    RemoteServiceRequestResponse,
+    Utils
 }
-
-// Setup the API client
-const api = new ConnectedDrive(process.env.BMW_CONNECTED_DRIVE_USERNAME ?? "", process.env.BMW_CONNECTED_DRIVE_PASSWORD ?? "", Regions.RestOfWorld, new FileTokenStore(), new ConsoleLogger());
-
-// Fetch a list of vehicles associated with the credentials
-api.getVehicles().then(vehicles => {
-    console.log(vehicles)
-    api.getVehicleStatus(vehicles[0].vin).then(v => {
-        // console.log(v);
-        // console.log(v.doorsState.combinedSecurityState);
-        // console.log(v.doorsState.combinedState);
-        console.log(v.currentMileage);
-        console.log(v.range);
-        console.log(v.electricChargingState.range);
-        console.log(v.combustionFuelLevel);
-        console.log(v.combustionFuelLevel.remainingFuelLiters);
-        console.log(v.combustionFuelLevel.range);
-    });
-    // api.getVehicleCapabilities(vehicles[0].vin).then(v => console.log(v))
-    // api.startClimateControl(vehicles[0].vin, true).then(response => console.log(response))
-    // api.blowHorn(vehicles[0].vin, true).then(response => console.log(response))
-}).catch(err => console.error(err));
